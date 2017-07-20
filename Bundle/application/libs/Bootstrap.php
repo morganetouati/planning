1c8a1d71c6d8c67bc0d4424767b989864d3eda26
morganetouati <morgane.touati@dartybox.com>
2017-07-20 10:50:20 +0200

    header et routing change

--- Bundle/application/libs/Bootstrap.php
+++ Bundle/application/libs/Bootstrap.php
@@ -1,29 +1,33 @@
 <?php
 
 class Bootstrap {
-
     function __construct() {
-        if (!isset($_GET['url'])) {
-            echo 'url not found';
+        isset($_GET['url']) ? $_GET['url'] : null;
+        $url = explode('/', filter_var(rtrim($_GET['url'], '/')), FILTER_SANITIZE_URL);
+        print_r($url);
+
+        if (empty($url[0])) {
+            require CONTROLLER_PATH . 'index.php'; //si url est vide redirection sur controllers/index.php
+            $controller = new Index(); //instantiation d'un nouvel index
+            return FALSE;
+        }
+
+        $file = CONTROLLER_PATH . $url[0] . '.php';
+        if (file_exists($file)) {
+            require $file;
         } else {
-            $url = explode('/', filter_var(rtrim($_GET['url'], '/')), FILTER_SANITIZE_URL);
-            $file = CONTROLLER_PATH . $url[0] . '.php';
-            if (file_exists($file)) {
-                require $file;
-            } else {
-                require CONTROLLER_PATH . 'error.php';
-                $controller = new Error();
-                return false;
-            }
+            require CONTROLLER_PATH . 'error.php';
+            $controller = new Error();
+            return false;
+        }
 
-            $controller = new $url[0];
+        $controller = new $url[0];
 
-            if (isset($url[2])) {
-                $controller->{$url[1]}($url[2]);
-            } else {
-                if (isset($url[1])) {
-                    $controller->{$url[1]}();
-                }
+        if (isset($url[2])) {
+            $controller->{$url[1]}($url[2]);
+        } else {
+            if (isset($url[1])) {
+                $controller->{$url[1]}();
             }
         }
     }
--- Bundle/application/views/header.php
+++ Bundle/application/views/header.php
@@ -1,8 +1,25 @@
+<?php
+$front = require '../../../front.php';
+?>
 <html>
     <head>
-        <title>title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="<?php URLRESOURCE . 'bootstrap.min.css' ?>">
+        <link rel="stylesheet" href="<?php URLRESOURCE . 'style.css' ?>">
+        <script type="text/javascript" src="<?php URLRESOURCE . 'jquery-3.1.1.min.js' ?>"></script>
+        <script src="<?php URLRESOURCE . 'bootstrap.min.js' ?>"></script>
+        <title>Planning</title>
     </head>
     <body>
-
+        <header>
+            <h1>Bienvenue sur la plateforme de gestion de présence des employés</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Inscription</a></li>
+                    <li><a href="">Connexion</a></li>
+                </ul>
+            </nav>
+        </header>
     </body>
 </html>
